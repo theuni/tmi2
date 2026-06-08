81ee27f8a852f32608a65f96dad84b217373bd00
rustaceanrob <rob.netzke@gmail.com>
2026-06-08 12:27:27 +0100

    Add `ctest` integration

--- test/test.cpp
+++ test/test.cpp
@@ -1,12 +1,37 @@
 #include <test/hashed_non_unique.h>
 #include <test/hashed_unique.h>
-#include <test/ordered_unique.h>
 #include <test/ordered_non_unique.h>
+#include <test/ordered_unique.h>
 
-int main()
-{
+#include <string_view>
+
+enum ExitStatus { Success = 0, Failure = 1 };
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
     test_hashed_non_unique();
     test_hashed_unique();
     test_ordered_unique();
     test_ordered_non_unique();
+    return ExitStatus::Success;
+  }
+
+  std::string_view name{argv[1]};
+  if (name == "hashed_non_unique") {
+    test_hashed_non_unique();
+    return ExitStatus::Success;
+  }
+  if (name == "hashed_unique") {
+    test_hashed_unique();
+    return ExitStatus::Success;
+  }
+  if (name == "ordered_unique") {
+    test_ordered_unique();
+    return ExitStatus::Success;
+  }
+  if (name == "ordered_non_unique") {
+    test_ordered_non_unique();
+    return ExitStatus::Success;
+  }
+  return ExitStatus::Failure;
 }
